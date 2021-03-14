c1607a8a7e4dbe40613ba8a94d1ed7c55183e169
pneves1051 <79472414+pneves1051@users.noreply.github.com>
2021-03-14 10:20:18 -0300

    Create dataset.py

---
+++ utils/dataset.py
@@ -0,0 +1,115 @@
+import os
+import glob
+
+import numpy as np
+import pandas as pd
+
+import torch
+import torchaudio
+torchaudio.set_audio_backend("sox_io")
+import torch.nn as nn
+
+
+class MusicDataset(torch.utils.data.IterableDataset):
+  def __init__(self, dataset_dir, audio_path, metadata_path, sr, window_size, hop_length, batch_size, transform=None):
+    """
+    Args:
+      song_folder (string): Caminho para a pasta com os arquivos.
+    """
+    self.dataset_dir = dataset_dir
+    self.file_list = glob.glob(dataset_dir + audio_path)
+    self.metadata = pd.read_csv(metadata_path).set_index('uuid4')
+    self.classes = self.metadata['instrument'].unique().tolist()
+    print(self.classes)
+
+    self.sr = sr
+    self.window_size = window_size
+    self.hop_length = hop_length
+    self.batch_size = batch_size
+    self.transform = transform
+
+    self.start = 0
+    self.end = len(self.file_list)
+    
+  @property
+  def shuffled_file_list(self):
+    shuffled_list = self.file_list.copy()
+    random.shuffle(shuffled_list)
+    return shuffled_list
+ 
+  def read_dataset(self, file_list, batch_size):
+    shuffled_list = file_list.copy()
+    random.shuffle(shuffled_list)    
+    ids=[]
+    audio = []
+    conditions = []
+    for f in shuffled_list:
+      music_id = os.path.basename(str(f)).replace('.wav', '').split('_')[-1]
+      if music_id != '':
+                
+        # carrega arquivo de áudio
+        #samplerate, signal = wavfile.read(f)
+        #signal = np.transpose(signal, axes=(0,1))
+        #signal = torch.Tensor(signal).transpose(0,1)
+        signal, orig_sr = torchaudio.load(f)
+        #signal = torchaudio.transforms.Resample(orig_sr, self.sr)(signal)
+        '''
+        with warnings.catch_warnings():
+          warnings.simplefilter("ignore")
+          signal, _ = librosa.load(file, sr=self.sr)
+        '''
+        # normalização entre [-1, 1]
+        signal = signal - signal.mean()
+        signal = signal/signal.abs().max()
+        
+        # mu scale
+        #signal = librosa.mu_compress(signal, quantize=False)[np.newaxis, ...]
+        #signal = 2*((torchaudio.transforms.MuLawEncoding(256)(signal) + 1)/256.) -1.
+        #signal = torchaudio.transforms.MuLawDecoding()(torchaudio.transforms.MuLawEncoding()(signal))
+
+        #assert not torch.any(signal.abs() > 1.)
+        signal = signal.mean(0, keepdim=True)
+        
+        
+        # condicionando por instrumento
+        cond = torch.Tensor(np.zeros(len(self.classes)))
+        sig_class = self.metadata.loc[music_id, 'instrument']
+        sig_class_idx = self.classes.index(sig_class)
+        cond[sig_class_idx] = 1.
+                             
+        ids.append(torch.Tensor([0]))
+        audio.append(signal)
+        conditions.append(cond)
+
+        if len(audio) >= batch_size:
+          #all_data = list(zip(ids, audio, conditions))
+          #random.shuffle(all_data)
+          #ids, audio, conditions = zip(*all_data)
+          
+          data_batch = {'ids': [], 'inputs': [], 'conditions': []}
+          data_batch['ids'] = torch.stack(ids)
+          data_batch['inputs'] = torch.stack(audio)
+          data_batch['conditions'] = torch.stack(conditions)
+                      
+          ids=[]
+          audio = []
+          conditions = []
+          
+          if self.transform:
+            data_batch = self.transform(data_batch)
+
+          yield data_batch        
+ 
+  def __iter__(self):
+    worker_info = torch.utils.data.get_worker_info()
+    if worker_info is None:
+      iter_start = self.start
+      iter_end = self.end
+    else:
+      # divides workload
+      per_worker = int(math.ceil((self.end-self.start)/ float(worker_info.num_workers)))
+      worker_id = worker_info.id
+      iter_start = self.start + worker_id * per_worker
+      iter_end = min(iter_start + per_worker, self.end)
+    return self.read_dataset(self.file_list[iter_start: iter_end],self.batch_size)
+  
